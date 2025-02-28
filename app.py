222e806e14b8d42280abc20d9309bd801432e2d6
jwymandev <jake@wyman.dev>
2025-02-28 22:28:09 +0100

    added Routers folder

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 import asyncpg
 import ssl
 from fastapi import FastAPI
-from location_router import router as location_router
-from profile_router import router as profile_router
+from routers.location_router import router as location_router
+from routers.profile_router import router as profile_router
 from config import get_db_config, get_ssl_context
 
 app = FastAPI()
